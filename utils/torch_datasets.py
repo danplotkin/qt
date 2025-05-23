ecf4f6b8c342f2b76418f67ac0c44e7cf3e22492
Daniel Plotkin <danmplotkin@gmail.com>
2025-05-22 21:05:09 -0500

    Added some basic work around

--- utils/torch_datasets.py
+++ utils/torch_datasets.py
@@ -5,23 +5,18 @@ from tqdm import tqdm
 
 
 class MiniPileDataset(Dataset):
-    def __init__(self, tokenized_ds: HFDataset, block_size: int = 512):
+    def __init__(self, path: str, block_size: int, stride: int = None):
+        self.tokens = torch.load(path)
         self.block_size = block_size
-        self.ds = tokenized_ds
-        self.indices = []
-
-        # Precompute (example_idx, start_pos) pairs
-        for i, example in enumerate(tqdm(tokenized_ds, desc="Initalizing Dataset...")):
-            input_ids = example["input_ids"]
-            for j in range(0, len(input_ids) - block_size, block_size):
-                self.indices.append((i, j))
+        self.stride = stride if stride is not None else block_size
+        self.indices = list(range(0, len(self.tokens) - block_size - 1, self.stride))
 
     def __len__(self):
         return len(self.indices)
 
     def __getitem__(self, idx):
-        example_idx, start_pos = self.indices[idx]
-        input_ids = self.ds[example_idx]["input_ids"][start_pos : start_pos + self.block_size + 1]
-        input_tensor = torch.tensor(input_ids[:-1], dtype=torch.long)
-        label_tensor = torch.tensor(input_ids[1:], dtype=torch.long)
+        start = self.indices[idx]
+        chunk = self.tokens[start : start + self.block_size + 1]
+        input_tensor = torch.tensor(chunk[:-1].clone(), dtype=torch.long)
+        label_tensor = torch.tensor(chunk[1:].clone(), dtype=torch.long)
         return {"input_ids": input_tensor, "labels": label_tensor}
