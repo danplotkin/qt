e58097dea03c2f52e413d894de5d2bd87d811cf6
Daniel Plotkin <danmplotkin@gmail.com>
2025-05-26 14:51:21 -0500

    Edit dataset class

--- utils/torch_datasets.py
+++ utils/torch_datasets.py
@@ -5,11 +5,12 @@ from tqdm import tqdm
 
 
 class MiniPileDataset(Dataset):
-    def __init__(self, path: str, block_size: int, stride: int = None):
+    def __init__(self, path: str, block_size: int, stride: int = None, offset: int = 0):
         self.tokens = torch.load(path)
+        self.offset = offset
         self.block_size = block_size
         self.stride = stride if stride is not None else block_size
-        self.indices = list(range(0, len(self.tokens) - block_size - 1, self.stride))
+        self.indices = list(range(0 + offset, len(self.tokens) - block_size, self.stride))
 
     def __len__(self):
         return len(self.indices)
@@ -17,6 +18,6 @@ class MiniPileDataset(Dataset):
     def __getitem__(self, idx):
         start = self.indices[idx]
         chunk = self.tokens[start : start + self.block_size + 1]
-        input_tensor = torch.tensor(chunk[:-1].clone(), dtype=torch.long)
-        label_tensor = torch.tensor(chunk[1:].clone(), dtype=torch.long)
+        input_tensor = chunk[:-1].clone().long()
+        label_tensor = chunk[1:].clone().long()
         return {"input_ids": input_tensor, "labels": label_tensor}
